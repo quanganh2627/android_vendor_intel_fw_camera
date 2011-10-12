67dbdab20c099550ae06867e15ca344a5347d40c
Vinod Govindapillai <vinod.govindapillai@intel.com>
2011-10-12 18:27:29 +0300

    Acceleration firmware binary for Face Acceleration for FR/FD/FT
    BZ: 12043
    
    Added the acceleration firmware binary needed for face acceleration
    feature to be used for face recognition/tracking/detection. The
    binary will be loaded to "/system/etc/firmware/" folder in the device.
    
    Change-Id: I0378992f2ce665e8eae7b5c784606570603671d8
    Signed-off-by: Vinod Govindapillai <vinod.govindapillai@intel.com>
    Reviewed-on: http://android.intel.com:8080/21166
    Reviewed-by: Koski, Anttu <anttu.koski@intel.com>
    Tested-by: Koski, Anttu <anttu.koski@intel.com>
    Reviewed-by: Kruger, Jozef <jozef.kruger@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -6,4 +6,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/shisp.bin:system/etc/firmware/shisp.bin
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/dis_cam14mp.bin:system/etc/firmware/dis_cam14mp.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/fa_extension.bin:system/etc/firmware/fa_extension.bin
 
