8c31b609de9d0d9ece99e68670831d9ee3adb50a
Aleksandar Sutic <aleksandar.sutic@intel.com>
2011-11-08 18:48:18 +0100

    camerafw: Acc FW for the HDR composition
    BZ: 17046
    
    This patch brings ISP standalone firmwares for all components
    required by the HDR image composition. These components are
    warping, deghosting, and fusion. The patch also includes
    simple image inversion and vividness enhancement firmwares
    for standalone acceleration testing purposes.
    
    Change-Id: I1d3f8526ef51d649a20fef7430f531291ba17da6
    Signed-off-by: Aleksandar Sutic <aleksandar.sutic@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,11 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/shisp.bin:system/etc/firmware/shisp.bin
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/dis_cam14mp.bin:system/etc/firmware/dis_cam14mp.bin
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/fa_extension.bin:system/etc/firmware/fa_extension.bin
-
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/isp_acc_warping.bin:system/etc/firmware/isp_acc_warping.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/isp_acc_deghosting.bin:system/etc/firmware/isp_acc_deghosting.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/isp_acc_fusion.bin:system/etc/firmware/isp_acc_fusion.bin
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/isp_acc_invert.bin:system/etc/firmware/isp_acc_invert.bin
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/isp_acc_vividness.bin:system/etc/firmware/isp_acc_vividness.bin
