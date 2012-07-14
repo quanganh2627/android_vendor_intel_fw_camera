7516b4ec89832f78cd209a5e92b5dfb47fde5b43
Bruce Beare <bruce.j.beare@intel.com>
2012-07-14 00:03:11 -0700

    PRODUCT_COPY_FILES: Disable incorrect use
    BZ: 46719
    
    This makfile variable is only able to be set in "product.mk" files -- never
    in Android.mk or AndroidBoard.mk files.
    
    Change-Id: I547d30b41d4746f41cd91e7bd07fe819b5ffc590

--- Android.mk
+++ Android.mk
@@ -1,15 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/shisp.bin:system/etc/firmware/shisp.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/dis_cam14mp.bin:system/etc/firmware/dis_cam14mp.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/fa_extension.bin:system/etc/firmware/fa_extension.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/isp_acc_multires.bin:system/etc/firmware/isp_acc_multires.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/isp_acc_warp_deghost.bin:system/etc/firmware/isp_acc_warp_deghost.bin
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/isp_acc_fusion.bin:system/etc/firmware/isp_acc_fusion.bin
+$(info WARNING: PRODUCT_COPY_FILES disabled in $(LOCAL_PATH))
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/shisp.bin:system/etc/firmware/shisp.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/dis_cam14mp.bin:system/etc/firmware/dis_cam14mp.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/fa_extension.bin:system/etc/firmware/fa_extension.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/isp_acc_multires.bin:system/etc/firmware/isp_acc_multires.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/isp_acc_warp_deghost.bin:system/etc/firmware/isp_acc_warp_deghost.bin
+#PRODUCT_COPY_FILES += \
+#	$(LOCAL_PATH)/isp_acc_fusion.bin:system/etc/firmware/isp_acc_fusion.bin
