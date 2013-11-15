6c8dc98f35b300d9cb025d78235e52228be0da99
Dalibor Segan <dalibor.segan@intel.com>
2013-11-15 14:33:22 +0100

    camerafw [HDR]: Added HDR v2.0 firmware for CSS2.1 baseline
    BZ: 147923
    
     - CSS2.1 FW is compiled against master_20131116_2201 baseline.
     - A new FW for CSS2.0 introduce the latest performance and
       IQ improvements.
     - Added PHONY packages in Android.mk file. HDR FW for each CSS and
       ISP version is now grouped in appropriate package which makes
       easier including this HDR FW in product make file.
     - Deprecated FW is removed from Android.mk file.
     - viedpp-ispfw Git HEAD: 08d14c1b527cff73070dc787815fa700879423dc
     - viedpp-libs  Git HEAD: 40173134a845a58f3a7350e3b5ac232359df1e6b
    
    Change-Id: I963fd9dcc1a3009b8741a8a02d26342ec2f3f174
    Signed-off-by: Dalibor Segan <dalibor.segan@intel.com>

--- Android.mk
+++ Android.mk
@@ -90,6 +90,23 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+##################################################
+# HDR FW CSS1.5 ISP2300
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hdr_fw_css15_2300
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+    isp_acc_multires_css15.bin \
+    isp_acc_warping_css15.bin \
+    isp_acc_deghosting_css15.bin \
+    isp_acc_fusion_css15.bin \
+    isp_acc_postproc_css15.bin
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := isp_acc_multires_css15.bin
 LOCAL_MODULE_OWNER := intel
@@ -134,6 +151,23 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
+
+##################################################
+# HDR FW CSS2.0 ISP2400A0
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hdr_fw_css20_2400
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+    isp_acc_multires_css20_2400.bin \
+    isp_acc_warping_css20_2400.bin \
+    isp_acc_deghosting_css20_2400.bin \
+    isp_acc_fusion_css20_2400.bin \
+    isp_acc_postproc_css20_2400.bin
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := isp_acc_multires_css20_2400.bin
@@ -180,6 +214,23 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
+##################################################
+# HDR FW CSS2.0 ISP2400B0
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hdr_fw_css20_2400b0
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+    isp_acc_multires_css20_2400b0.bin \
+    isp_acc_warping_css20_2400b0.bin \
+    isp_acc_deghosting_css20_2400b0.bin \
+    isp_acc_fusion_css20_2400b0.bin \
+    isp_acc_postproc_css20_2400b0.bin
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := isp_acc_multires_css20_2400b0.bin
 LOCAL_MODULE_OWNER := intel
@@ -225,50 +276,22 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := isp_acc_multires_v2_css20_2400.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+##################################################
+# HDR v2 FW CSS2.0 ISP2400B0
+##################################################
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := isp_acc_warping_v2_css20_2400.bin
-LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE := hdr_v2_fw_css20_2400b0
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+LOCAL_REQUIRED_MODULES := \
+    isp_acc_multires_v2_css20_2400b0.bin \
+    isp_acc_warping_v2_css20_2400b0.bin \
+    isp_acc_deghosting_v2_css20_2400b0.bin \
+    isp_acc_lumaproc_css20_2400b0.bin \
+    isp_acc_chromaproc_css20_2400b0.bin
+include $(BUILD_PHONY_PACKAGE)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := isp_acc_deghosting_v2_css20_2400.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := isp_acc_lumaproc_css20_2400.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := isp_acc_chromaproc_css20_2400.bin
-LOCAL_MODULE_OWNER := intel
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
+##################################################
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := isp_acc_multires_v2_css20_2400b0.bin
@@ -308,6 +331,68 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := isp_acc_chromaproc_css20_2400b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+##################################################
+# HDR v2 FW CSS2.1 ISP2400B0
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := hdr_v2_fw_css21_2400b0
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := \
+    isp_acc_multires_v2_css21_2400b0.bin \
+    isp_acc_warping_v2_css21_2400b0.bin \
+    isp_acc_deghosting_v2_css21_2400b0.bin \
+    isp_acc_lumaproc_css21_2400b0.bin \
+    isp_acc_chromaproc_css21_2400b0.bin
+include $(BUILD_PHONY_PACKAGE)
+
+##################################################
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := isp_acc_multires_v2_css21_2400b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := isp_acc_warping_v2_css21_2400b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := isp_acc_deghosting_v2_css21_2400b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := isp_acc_lumaproc_css21_2400b0.bin
+LOCAL_MODULE_OWNER := intel
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/firmware
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := isp_acc_chromaproc_css21_2400b0.bin
 LOCAL_MODULE_OWNER := intel
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
