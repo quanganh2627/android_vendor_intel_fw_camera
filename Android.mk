91ae5917fcefc7f3e4dc25adbeb2353eba7d3e67
Bin Han <bin.b.han@intel.com>
2014-01-28 15:41:02 +0800

    camerafw: remove unused firmware
    BZ: 151275
    
    For current PSI mainline, we have some FW/CSS are actually dead code because of
    some reasons listed below.
    
    Reasons:
    1   CSS from master_byt branch is phased out as we have switched to CSS master
    branch for all except RHB
    2   ISP2400A0 is not supported any more.
    3   we are not going to have accelerated face recognition for ISP2400
    4   For HDR, we have switched to HDR v2.0 for ISP2400B0. So, some the V1 HDR
    firmware on ISP2400 can be removed like isp_acc_multires_css20_2400b0.bin
    
    So here is the list of all the items(that I can think of) to be removed,
    
    ISP FW:
    shisp_2400_cssv2_isp_2_2.bin  --- used by ISP2400A0 with ISP2.2 FW CSS master_byt branch
    shisp_2400a0_v21.bin  --- used by ISP2400A0 with CSS master branch
    shisp_2400_cssv2.bin  --- used by ISP2400A0 with CSS master_byt branch
    shisp_2400b0_cssv2.bin --- used by ISP2400B0 with CSS master_byt branch
    
    FR FW:
    isp_fr_2400.bin/isp_fr_2400B0.bin  ---  Face recognition FW used on ISP2400
    A0/B0, but because acceleration version FR is not going to happen on MERR/BYT,
    so they can be removed
    
    HDR FW:
    isp_acc_XXXXX_(v2)_css20_2400.bin  ---  HDR firmware working on CSS master_byt branch for ISP2400A0
    isp_acc_XXXXX_(v2)_css20_2400b0.bin  ---  HDR firmware working on CSS master_byt branch for ISP2400B0
    
    Change-Id: I4ba54b603192da92966e14ee06c9631ce50b24c8
    Signed-off-by: Bin Han <bin.b.han@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,21 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 
 # ISP and FR
-camera_libs := shisp_css15.bin shisp_2400b0_cssv2.bin shisp_2400_cssv2.bin shisp_2400_cssv2_isp_2_2.bin shisp_2400a0_v21.bin shisp_2400b0_v21.bin \
-shisp_2401a0_legacy_v21.bin shisp_2401a0_v21.bin \
-isp_fr.bin isp_fr_2400.bin
+camera_libs := shisp_css15.bin shisp_2400b0_v21.bin shisp_2401a0_legacy_v21.bin shisp_2401a0_v21.bin isp_fr.bin
 # HDR FW CSS1.5 ISP2300
 hdr_css15_2300_libs := isp_acc_multires_css15.bin isp_acc_warping_css15.bin isp_acc_deghosting_css15.bin \
 isp_acc_fusion_css15.bin isp_acc_postproc_css15.bin
-# HDR FW CSS2.0 ISP2400A0
-hdr_css20_2400_libs := isp_acc_multires_css20_2400.bin isp_acc_warping_css20_2400.bin isp_acc_deghosting_css20_2400.bin \
-isp_acc_fusion_css20_2400.bin isp_acc_postproc_css20_2400.bin
-# HDR FW CSS2.0 ISP2400B0
-hdr_css20_2400b0_libs := isp_acc_multires_css20_2400b0.bin isp_acc_warping_css20_2400b0.bin isp_acc_deghosting_css20_2400b0.bin \
-isp_acc_fusion_css20_2400b0.bin isp_acc_postproc_css20_2400b0.bin
-# HDR v2 FW CSS2.0 ISP2400B0
-hdr_v2_css20_2400b0_libs := isp_acc_multires_v2_css20_2400b0.bin isp_acc_warping_v2_css20_2400b0.bin isp_acc_deghosting_v2_css20_2400b0.bin \
-isp_acc_lumaproc_css20_2400b0.bin isp_acc_chromaproc_css20_2400b0.bin
 # HDR v2 FW CSS2.1 ISP2400B0
 hdr_v2_css21_2400b0_libs := isp_acc_multires_v2_css21_2400b0.bin isp_acc_warping_v2_css21_2400b0.bin isp_acc_deghosting_v2_css21_2400b0.bin \
 isp_acc_lumaproc_css21_2400b0.bin isp_acc_chromaproc_css21_2400b0.bin
@@ -49,33 +38,6 @@ include $(BUILD_PHONY_PACKAGE)
 
 $(call camera-prebuilt-boilerplate, \
     $(hdr_css15_2300_libs))
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := hdr_fw_css20_2400
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(hdr_css20_2400_libs)
-include $(BUILD_PHONY_PACKAGE)
-
-$(call camera-prebuilt-boilerplate, \
-    $(hdr_css20_2400_libs))
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := hdr_fw_css20_2400b0
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(hdr_css20_2400b0_libs)
-include $(BUILD_PHONY_PACKAGE)
-
-$(call camera-prebuilt-boilerplate, \
-    $(hdr_css20_2400b0_libs))
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := hdr_v2_fw_css20_2400b0
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(hdr_v2_css20_2400b0_libs)
-include $(BUILD_PHONY_PACKAGE)
-
-$(call camera-prebuilt-boilerplate, \
-    $(hdr_v2_css20_2400b0_libs))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hdr_v2_fw_css21_2400b0
